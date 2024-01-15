63854a9ee29221fc170fa305b4e2c10585fd661d
dinhnam72 <dinhnam20162011@gmail.com>
2024-01-15 19:09:53 +0700

    update login 19h 15/01

---
+++ medical_supplies/src/components/register/Register.js
@@ -0,0 +1,215 @@
+import {ErrorMessage, Field, Formik, Form} from "formik";
+import {useEffect, useState} from "react";
+import * as Yup from "yup";
+import * as accountService from "../../services/accounts/AccountService";
+import {toast} from "react-toastify";
+import {Navigate, useNavigate, NavLink} from "react-router-dom";
+
+
+export default function Register() {
+    const [roles, setRoles] = useState([]);
+    const navigate = useNavigate();
+    useEffect(() => {
+        getAllRole();
+    }, []);
+
+    const getAllRole = async () => {
+        try {
+            const res = await accountService.roleList();
+            setRoles(res.data)
+        } catch (e) {
+            throw e.response;
+        }
+    }
+
+    const initValues = {
+        email: "",
+        password: "",
+        name: "",
+        birthday: "",
+        phone: "",
+        address: "",
+        gender: "",
+        idRole: ""
+
+    }
+    const validateFormRegister = {
+        email: Yup.string()
+            .required("Vui lòng nhập email."),
+        password: Yup.string()
+            .required("Vui lòng nhập mật khẩu."),
+        name: Yup.string()
+            .required("Vui lòng nhập tên."),
+        birthday: Yup.string()
+            .required("Vui lòng nhập ngày sinh."),
+        phone: Yup.string()
+            .required("Vui lòng nhập số điện thoại."),
+        address: Yup.string()
+            .required("Vui lòng nhập địa chỉ."),
+        idRole: Yup.string()
+            .required("Vui lòng chọn chức vụ.")
+    };
+
+    const handleSubmitFormRegister = async (values, setFieldError) => {
+        console.log(values);
+        try {
+            const res = await accountService.createAccount(values);
+            if (res.status === 200) {
+                navigate("/register")
+                toast.success("Đăng ký thành công !");
+
+            }
+        } catch (e) {
+            console.log(e)
+            setFieldError("password", e.data);
+
+        }
+    }
+    if (!roles) return null;
+    return (
+        <>
+
+            <div className="main">
+                <nav className="navbar navbar-expand px-3 border-bottom">
+                    <button className="btn btn-sm" type="button" data-bs-theme="dark">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                </nav>
+                <main className="content px-3 py-2">
+                    <div className="container-fluid">
+                        <div className="row content">
+                            <div className="col-lg-12 pt-lg" style={{paddingLeft: '15%'}}>
+                                <div className="col-md-9 col-lg-9 col-xl-9">
+                                    <div className="d-flex justify-content-center">
+                                        <div className="form-control shadow rounded-0 p-4"
+                                             style={{backgroundImage: "url('/imgage/yte4.png')"}}>
+                                            <h2 className="text-center">Tạo Tài Khoản</h2>
+                                            <Formik initialValues={initValues}
+                                                    onSubmit={(values, {setFieldError}) => handleSubmitFormRegister(values, {setFieldError})}
+                                                    validationSchema={Yup.object(validateFormRegister)}
+                                            >
+                                                <Form className="mt-3">
+                                                    <div className="mb-3">
+                                                        <label htmlFor="email"
+                                                               className="form-label fw-bold">Email<span
+                                                            className="text-danger">(*)</span></label>
+                                                        <Field type="text" className="form-control" id="email"
+                                                               name="email"
+                                                               aria-describedby="emailHelp"/>
+                                                    </div>
+                                                    <ErrorMessage name="email" className="text-danger"
+                                                                  component="p"/>
+                                                    <div className="mb-3">
+                                                        <label htmlFor="password" className="form-label fw-bold">Mật
+                                                            khẩu<span
+                                                                className="text-danger">(*)</span></label>
+                                                        <Field type="password" className="form-control"
+                                                               id="password"
+                                                               name="password"/>
+                                                    </div>
+                                                    <ErrorMessage name="password" className="text-danger"
+                                                                  component="p"/>
+                                                    <div className="mb-3">
+                                                        <label htmlFor="idRole" className="form-label fw-bold">Chức
+                                                            vụ<span
+                                                                className="text-danger">(*)</span></label>
+                                                        < Field as="select" className="form-select" name="idRole">
+                                                            <option>-----Chọn nghiệp vụ-----</option>
+                                                            {
+                                                                roles.map(role => (
+                                                                    <option key={role.idRole} value={role.idRole}>
+                                                                        {role.idRole === 1 ?
+                                                                            "Admin" : role.idRole === 2 ?
+                                                                                "Kế toán" : "Người bán hàng"}
+                                                                    </option>
+                                                                ))
+                                                            }
+                                                        </Field>
+                                                    </div>
+                                                    <ErrorMessage name="idRole" className="text-danger"
+                                                                  component="p"/>
+                                                    <div className="mb-3">
+                                                        <label htmlFor="name" className="form-label fw-bold">Tên
+                                                            nhân
+                                                            viên<span
+                                                                className="text-danger">(*)</span></label>
+                                                        <Field type="text" className="form-control" id="name"
+                                                               name="name"/>
+                                                    </div>
+                                                    <ErrorMessage name="name" className="text-danger"
+                                                                  component="p"/>
+                                                    <div className="mb-3">
+                                                        <label htmlFor="birthday" className="form-label fw-bold">Ngày
+                                                            sinh<span
+                                                                className="text-danger">(*)</span></label>
+                                                        <Field type="date" className="form-control" id="birthday"
+                                                               name="birthday"/>
+                                                    </div>
+                                                    <ErrorMessage name="birthday" className="text-danger"
+                                                                  component="p"/>
+                                                    <div className="mb-3">
+                                                        <label className="form-label fw-bold">Giới tính<span
+                                                            className="text-danger">(*)</span></label>
+                                                        <div>
+                                                            <Field className="form-check-input" type="radio"
+                                                                   name="gender"
+                                                                   id="nam" value={true}
+                                                                   checked/>
+                                                            <label className="form-check-label" htmlFor="nam">
+                                                                Nam
+                                                            </label>
+
+                                                            <Field className="form-check-input" type="radio"
+                                                                   name="gender"
+                                                                   id="nu" value={false}/>
+                                                            <label className="form-check-label" htmlFor="nu">
+                                                                Nữ
+                                                            </label>
+                                                        </div>
+                                                    </div>
+                                                    <div className="mb-3">
+                                                        <label htmlFor="address" className="form-label fw-bold">Địa
+                                                            chỉ<span
+                                                                className="text-danger">(*)</span></label>
+                                                        <Field as="textarea" className="form-control" id="address"
+                                                               name="address"></Field>
+                                                    </div>
+                                                    <ErrorMessage name="address" className="text-danger"
+                                                                  component="p"/>
+                                                    <div className="mb-3">
+                                                        <label htmlFor="phone" className="form-label fw-bold">
+                                                            Số điện thoại
+                                                            <span className="text-danger">(*)</span></label>
+                                                        <Field type="number" className="form-control" id="phone"
+                                                               name="phone"/>
+                                                    </div>
+                                                    <ErrorMessage name="phone" className="text-danger"
+                                                                  component="p"/>
+                                                    <div className="row mt-5">
+                                                        <div className="col-6 d-flex justify-content-end">
+                                                            <NavLink to={"/home1"}
+                                                                     className="btn btn-secondary me-2">Trở
+                                                                về</NavLink>
+                                                        </div>
+
+                                                        <div className="col-6">
+                                                            <button type="submit"
+                                                                    className="btn btn-success ms-2">Thêm
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </Form>
+                                            </Formik>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </main>
+            </div>
+        </>
+    )
+}
---
+++ medical_supplies/src/services/auth/AuthService.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export default function authHeader(){
+    const user = JSON.parse(localStorage.getItem(`user`));
+    if (user && user.accessToken){
+        return{
+            "Authorization": 'Bearer ' + user.accessToken,
+            "Content-Type": 'application/json'
+        }
+    } else {
+        return {};
+    }
+}
+export const login = async (account)=>{
+    console.log(account)
+    try {
+        const res = await axios.post("http://localhost:8080/api/login",account);
+        return res;
+    }catch (e) {
+        console.log(e)
+        throw e.response;
+    }
+}
